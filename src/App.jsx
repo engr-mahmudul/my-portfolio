16116e69fd53165ba7f5862dd30fa2dcceebdf1b
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-03-17 12:04:59 +0200

    Project section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./sections/Hero";
 import MoreAbout from "./sections/MoreAbout";
+import Projects from "./sections/Projects";
 
 export default function App() {
   return (
     <>
       <Hero />
       <MoreAbout />
+      <Projects />
     </>
   );
 }
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,56 @@
+import dashBoard from "../assets/images/dash.jpg";
+import eCommerce from "../assets/images/ecom.jpg";
+import membership from "../assets/images/membership_site.jpg";
+export default function Projects() {
+  return (
+    <di className="s1">
+      <div className="main-container">
+        <h3 style={{ textAlign: "center" }}>Some of my past projects</h3>
+
+        <div className="post-wrapper">
+          <div>
+            <div className="post">
+              <img className="thumbnail" src={dashBoard} />
+              <div className="post-preview">
+                <h6 className="post-title">Laboratory Management System</h6>
+                <p className="post-intro">
+                  Designed built & mantained a the lab managment system for FOI
+                  Laboratories
+                </p>
+                <a href="post.html">Read More</a>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="post">
+              <img className="thumbnail" src={membership} />
+              <div className="post-preview">
+                <h6 className="post-title">Online Store - CoursePost Title</h6>
+                <p className="post-intro">
+                  Online store with paypal payments intergration and guest user
+                  shopping
+                </p>
+                <a href="post.html">Read More</a>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="post">
+              <img className="thumbnail" src={eCommerce} />
+              <div className="post-preview">
+                <h6 className="post-title">Membership Website</h6>
+                <p className="post-intro">
+                  Modulized guide for online courses with step by step
+                  intructions
+                </p>
+                <a href="post.html">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </di>
+  );
+}
